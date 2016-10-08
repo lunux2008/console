f9c3e989cff9d5c3be190ff87f1c34e56eba4eb1
luxu <luxu@btime.com>
2016-10-08 20:15:36 +0800

    example 增加读取CONF DEMO

--- example/commands/default.go
+++ example/commands/default.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"github.com/lunux2008/console"
+	"github.com/astaxie/beego"
 	"fmt"
 )
 
@@ -10,21 +11,32 @@ type MainCommand struct {
 }
 
 func (c *MainCommand) Handle() {
-
+	
+	// 字符
 	stringArg, _ := c.GetString("string", "def")
 	fmt.Println(stringArg)
-
+	
+	// 整形
 	intArg, _ := c.GetInt("age", 200)
 	fmt.Println(intArg)
 
+	// 布尔
 	boolArg, _ := c.GetBool("bool", false)
 	fmt.Println(boolArg)
-
+	
+	// 浮点
 	floatArg, _ := c.GetFloat("float", 1.1)
 	fmt.Println(floatArg)
 	
+	// SLICE
 	sliceArg := c.GetStrings("slice", []string{"c", "d", "e"})
 	fmt.Println(sliceArg)
+	
+	// 实用BEEGO读取CONF
+	fmt.Println(beego.AppConfig.String("appname"))
+	
+	// 查看当前运行环境
+	fmt.Println(beego.AppConfig.String("env"))
 }
 
 func (c *MainCommand) GetOptions() []*console.Option {
