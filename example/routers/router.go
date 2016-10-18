156499f608e8313b63896232a37dfe4bcaf64b56
luxu-so <luxu-so@360.cn>
2016-10-18 17:52:56 +0800

    add modules

--- example/routers/router.go
+++ example/routers/router.go
@@ -2,9 +2,11 @@ package routers
 
 import (
 	"github.com/lunux2008/console/example/commands"
+	module1_commands "github.com/lunux2008/console/example/modules/module1/commands"
 	"github.com/lunux2008/console"
 )
 
 func init() {
     console.Router("demo", &commands.MainCommand{})
+	console.Router("module1/demo", &module1_commands.MainCommand{})
 }
